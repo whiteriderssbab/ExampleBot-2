b70bfac27bf781738ca2f36c45b725455348c818
WhiteRider <53314775+whiteriderssbab@users.noreply.github.com>
2019-09-29 05:18:42 -0600

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,30 +1,25 @@
 const Discord = require("discord.js");
-module.exports.run = async (bot, message, args) => {
-    message.delete();
-    if(!message.member.hasPermission("BAN_MEMBERS")) return errors.noPerms(message, "BAN_MEMBERS");
-    if(args[0] == "help"){
-      message.reply("Usage: !ban <user> <reason>");
-      return;
-    }
-    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!bUser) return errors.cantfindUser(message.channel);
-    if(bUser.id === bot.user.id) return errors.botuser(message); 
-    let bReason = args.join(" ").slice(22);
-    if(!bReason) return errors.noReason(message.channel);
-    if(bUser.hasPermission("MANAGE_MESSAGES")) return errors.equalPerms(message, bUser, "MANAGE_MESSAGES");
 
+module.exports.run = async (bot, message, args) => {
+    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!bUser) return message.channel.send("Can't find user!");
+    let bReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You lack permission :x:");
+    if(bUser.hasPermission("BAN_MEMBERS")) return message.channel.send("That person can't be banned :x:");
+bUser.ban({
+          reason: bReason,
+        }).then(() => {
     let banEmbed = new Discord.RichEmbed()
-    .setDescription("~Ban~")
-    .setColor("#bc0000")
-    .addField("Banned User", `${bUser} with ID ${bUser.id}`)
-    .addField("Banned By", `<@${message.author.id}> with ID ${message.author.id}`)
-    .addField("Banned In", message.channel)
+    .setDescription("Ban Log")
+    .setColor("RANDOM")
+    .addField("Banned by:", `${message.author}`)
+    .addField("In server:", `${message.guild.name}`)
     .addField("Time", message.createdAt)
     .addField("Reason", bReason);
-  
-    message.guild.member(bUser).ban(bReason);
-   message.channel.send(banEmbed);
-  bUser.send(banEmbed)
+          
+message.channel.send(`${bUser} has been banned.`)
+    bUser.send(banEmbed);
+})
 }
 
 module.exports.help = {
